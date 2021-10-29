a6d25d4354c05639fbdec3f0dc15d68649d2e5be
ofir-zukerman <ofir.zukerman@gmail.com>
2021-10-29 17:04:52 +0300

    update remove component

--- src/components/DataTable/Table/Remove.jsx
+++ src/components/DataTable/Table/Remove.jsx
@@ -5,17 +5,15 @@ import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined
 
 const Remove = (props) => {
   return (
-    <div>
-      <TableCell key={props.fieldName}>
-        <IconButton>
-          <DeleteForeverOutlinedIcon
-            onClick={async () =>
-              await props.handleRemoveEmployee(props.employeeId)
-            }
-          />
-        </IconButton>
-      </TableCell>
-    </div>
+    <TableCell key={props.fieldName}>
+      <IconButton>
+        <DeleteForeverOutlinedIcon
+          onClick={async () =>
+            await props.handleRemoveEmployee(props.employeeId)
+          }
+        />
+      </IconButton>
+    </TableCell>
   );
 };
 
